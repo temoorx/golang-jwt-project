0e407aab9e4d5912481c79910d5fc93a8b5bf855
temoorx <taak140@gmail.com>
2024-01-13 17:40:41 +0500

    Add bcrypt package for password hashing and implement login functionality

--- controllers/userController.go
+++ controllers/userController.go
@@ -14,6 +14,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/net/context"
 )
 
@@ -58,25 +59,53 @@ func Signup() gin.HandlerFunc {
 		user.ID = primitive.NewObjectID()
 		user.User_id = user.ID.Hex()
 		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
-		user.Token= &token
-		user.Refresh_token= &refreshToken
+		user.Token = &token
+		user.Refresh_token = &refreshToken
 
-		
-		resultInsertionNumber,insertErr := userCollection.InsertOne(ctx, user)
-		if insertErr != nil{
-			msg :=fmt.Sprintf("User item was not created")
-			c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
 		defer cancel()
 		c.JSON(http.StatusOK, resultInsertionNumber)
 
-	}	
+	}
 }
-func Login(){
-	
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"email": "email or password is incorrect"})
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+	}
+
 }
-func VerifyPassword() {}
+func VerifyPassword(userPassword string, providedPassowrd string  ) (bool, string){
+	err := bcrypt.CompareHashAndPassword([]byte (providedPassowrd), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil{
+		msg= fmt.Sprint("email of password is not correct ")
+		check = false
+	}
+	return check, msg
+}
 
 func HashPassword() {}
 
