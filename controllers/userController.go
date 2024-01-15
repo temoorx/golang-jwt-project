1b2cfcc48b8b7e5cfddd7976592925f4b985832f
temoorx <temoorx@gmail.com>
2024-01-15 17:21:42 +0500

    Fix login error handling and unauthorized access

--- controllers/userController.go
+++ controllers/userController.go
@@ -101,37 +101,43 @@ func Login() gin.HandlerFunc {
 
 		if err := c.BindJSON(&user); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			cancel()
 			return
 		}
 
 		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
 		defer cancel()
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"email": "email or password is incorrect"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "email or password is incorrect"})
+			return
 		}
 
 		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
-		defer cancel()
-		if passwordIsValid != true {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		if !passwordIsValid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": msg})
 			return
 		}
+
 		if foundUser.Email == nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "user not found"})
+			return
 		}
+
 		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name, *foundUser.Last_name, *foundUser.User_type, foundUser.User_id)
 		helper.UpdateAllTokens(token, refreshToken, foundUser.User_id)
+
 		err = userCollection.FindOne(ctx, bson.M{"user_id": foundUser.User_id}).Decode(&foundUser)
 
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		// c.JSON(http.StatusOK, foundUser)
+
 		// Include a success message along with the user data
 		c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user": foundUser})
 	}
-
 }
+
 func VerifyPassword(userPassword string, providedPassowrd string) (bool, string) {
 	err := bcrypt.CompareHashAndPassword([]byte(providedPassowrd), []byte(userPassword))
 	check := true
